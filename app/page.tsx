616195c2516af89a5d413a8d094d1af2357a99dc
MarufPulok <maruf12@student.sust.edu>
2023-07-15 18:00:30 +0600

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import DrawingBoard from './components/DrawingBoard'
 import Canvas from './components/Canvas'
-import NewGrid from './components/NewGrid'
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center p-5">
+    <div className="flex items-center justify-center">
       <Canvas/>
     </div>
   )
