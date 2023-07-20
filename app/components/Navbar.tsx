2a03a39df0dd99d320f580d413f21cbc05fd8b93
anindo <sarkeranindo@gmail.com>
2023-07-21 05:10:33 +0600

    signout working

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,20 @@
 "use client";
 import React from "react";
-import { signOut } from "next-auth/react";
-import getCurrentUser from "../action/getCurrentUser";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push("/login");
+  };
+
   return (
     <div className="p-5 flex justify-end">
-      <button
-        className="p-3 bg-blue-600 rounded-md"
-        onClick={() => {
-          signOut();
-        }}
-      >
+      <button className="p-3 bg-blue-600 rounded-md" onClick={handleLogout}>
         Sign out
       </button>
     </div>
