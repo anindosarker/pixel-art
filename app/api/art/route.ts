517ade79693141a6939a67e48813eb52a1ee4f3d
MarufPulok <maruf12@student.sust.edu>
2023-07-20 15:37:00 +0600

    fix

--- app/api/art/route.ts
+++ app/api/art/route.ts
@@ -2,6 +2,10 @@ import Arts from "@/backend/models/Arts";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const arts = await Arts.find({}).populate('userId', 'username').sort({ createdAt: -1 }).lean().maxTimeMS(2000);
-    return NextResponse.json(arts);
+    try {
+        const arts = await Arts.find({}).populate('userId', 'username').sort({ createdAt: -1 }).lean().maxTimeMS(2000);
+        return NextResponse.json(arts);
+    } catch (error) {
+        return NextResponse.json({ error: "something went wrong" }, { status: 500 });
+    }
 }
