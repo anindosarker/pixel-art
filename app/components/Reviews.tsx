028482c7067237ada43e981968cbde33f3cad8de
anindo <sarkeranindo@gmail.com>
2023-07-22 01:46:36 +0600

    build error fix

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Database } from "@/lib/database.types";
 import Image from "next/image";
 import React from "react";
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,20 @@
 import Canvas from "./components/Canvas";
-import SignUp from "./components/SignUp";
 import Navbar from "./components/Navbar";
 import RenderArts from "./components/RenderArts";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "@/lib/database.types";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const supabase = createServerComponentClient<Database>({ cookies });
-
-  const currentUser = await supabase.auth.getUser();
-  console.log("👉️ ~ file: page.tsx:13 ~ Home ~ currentUser:\n", currentUser);
-
-  if (currentUser) {
-    return (
-      <>
-        <Navbar />
-        <div className="flex flex-col gap-5 items-center justify-center bg-black p-5">
-          <div className="text-2xl font-semibold text-rose-500">
-            Welcome {currentUser?.data.user?.email} <br />
-          </div>
-          <Canvas />
-          <div className="flex flex-col gap-4 w-1/2">
-            <RenderArts />
-          </div>
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col gap-5 items-center justify-center bg-black p-5">
+        <div className="text-2xl font-semibold text-rose-500">
+          Welcome to Pixel art
         </div>
-      </>
-    );
-  }
-  return <SignUp />;
+        <Canvas />
+        <div className="flex flex-col gap-4 w-1/2">
+          <RenderArts />
+        </div>
+      </div>
+    </>
+  );
 }
