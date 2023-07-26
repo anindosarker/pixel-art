4b9250105990ded0b7f954fcd08f3829fa7f84de
MarufPulok <maruf12@student.sust.edu>
2023-07-26 12:45:01 +0600

    pages route setup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ssikazrywogwlervpmgv.supabase.co",
-        port: "",
-      },
-    ],
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
