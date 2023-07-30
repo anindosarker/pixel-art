e545975405c3ab5ffbb4b2ba539eb9ace58a0f72
anindo <sarkeranindo@gmail.com>
2023-07-30 12:56:14 +0600

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,17 @@ import type { Database } from "@/lib/database.types";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   return res;
 }
+
+export const config = {
+  matcher: ["/"],
+};
